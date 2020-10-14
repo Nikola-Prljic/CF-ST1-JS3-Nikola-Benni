289b715875ae980e259cbd4674a4080b6b2fd246
Nikola <nik.7377@gmail.com>
2020-10-14 11:01:12 +0200

    adding e1

---
+++ nik/e1/scr.js
@@ -0,0 +1,18 @@
+onload=WarningMsg();
+function WarningMsg() {
+    alert("This is a warning message!");
+    document.getElementById('p1').innerHTML = ("This is a warning message!");
+     var retVal = confirm("Do you want to continue ?");
+     if(retVal == true){
+        document.getElementById('p2').innerHTML = ("User wants to continue!");    
+     }
+     else{
+        document.getElementById('p3').innerHTML = ("User does not want to continue!");    
+     }
+    }
+
+  onload=getValue(); 
+    function getValue(){
+        var retVal = prompt("Enter your name : " , "your name here");
+        document.getElementById('p4').innerHTML =("You have entered : " + retVal);
+       }
